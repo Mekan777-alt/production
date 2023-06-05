de5aa91d8f5ada155b980f0fdb5b96b48fcf82bb
Mekan777-alt <mekan2678@gmail.com>
2023-06-05 18:17:57 +0300

    add base.html

--- production/settings.py
+++ production/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main.apps.MainConfig',
+    'contact.apps.ContactConfig',
+    'works.apps.WorksConfig'
 ]
 
 MIDDLEWARE = [
